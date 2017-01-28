9dcee45bddcff0c1703599acd6eace68c4b93a46
ChrisBotCodes <chrisadambot@gmail.com>
2017-01-28 12:04:22 -0500

    Fix mongo connection string issue

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 "use strict";
 
 // Basic express setup:
-
+require('dotenv').config();
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
-const mongo_uri     = "mongodb://localhost:27017/tweeter";
+// const mongo_uri     = "mongodb://localhost:27017/tweeter";
+const mongo_uri     = process.env.mongodb_uri;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
